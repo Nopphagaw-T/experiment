e168b78c182476e728a5c4dd573cadba6ee1ff3c
nopphagaw <nopphagaw.t@fitm.kmutnb.ac.th>
2026-05-26 09:16:48 +0700

    Remove demo files for perimeter calculation, equation evaluation, and timer functionality

--- Demo/Ex1.js
+++
@@ -1,8 +0,0 @@
-findPerimeter = (length, width)=>{ return 2 * (length + width) }
-data = [[6, 7], [20, 10], [2,9]]
-
-data.forEach((item) => {
-  console.log(findPerimeter(...item))
-})
-
-//console.log(findPerimeter(6, 7))
--- Demo/dateobj.js
+++
@@ -1,8 +0,0 @@
-const start = Date.now();
-
-console.log('Starting timer...');
-
-setTimeout(() => {
-  const millis = Date.now() - start;
-  console.log(`secound elapsed = ${Math.floor(millis / 1000)}`);
-}, 5000);
